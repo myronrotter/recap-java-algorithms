d27105478f266cacc1a2da59d33cc5346106c494
Myron Rotter <myronrotter01@t-online.de>
2020-01-15 10:26:40 +0100

    Add sorting test interface; refactor test classes

--- src/test/java/com/algorithms/sorting/BogoSortTest.java
+++ src/test/java/com/algorithms/sorting/BogoSortTest.java
@@ -1,64 +1,11 @@
 package com.algorithms.sorting;
 
-import org.junit.Assert;
-import org.junit.Test;
+import com.algorithms.interfaces.Sorting;
 
-public class BogoSortTest {
-  BogoSort bogoSort = new BogoSort();
+class BogoSortTest implements SortingTest<Sorting> {
 
-  @Test
-  public void testPositiveIntegers() {
-    Integer[] arr = new Integer[] { 9, 6, 2, 3, 0, 4, 5, 1, 7, 8 };
-    Integer[] expected = new Integer[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
-    Assert.assertArrayEquals(expected, bogoSort.sort(arr));
-  }
-
-  @Test
-  public void testNegativeIntegers() {
-    Integer[] arr = new Integer[] { -9, -6, -2, -3, 0, -4, -5, -1, -7, -8 };
-    Integer[] expected = new Integer[] { -9, -8, -7, -6, -5, -4, -3, -2, -1, 0 };
-
-    Assert.assertArrayEquals(expected, bogoSort.sort(arr));
-  }
-
-  @Test
-  public void testMixedIntegers() {
-    Integer[] arr = new Integer[] { -50, 3, -1, 90, 77, 2, 5, 0, -100 };
-    Integer[] expected = new Integer[] { -100, -50, -1, 0, 2, 3, 5, 77, 90 };
-
-    Assert.assertArrayEquals(expected, bogoSort.sort(arr));
-  }
-
-  @Test
-  public void testDuplicateIntegers() {
-    Integer[] arr = new Integer[] { 0, 1, 0, 1, 0, 0 };
-    Integer[] expected = new Integer[] { 0, 0, 0, 0, 1, 1 };
-
-    Assert.assertArrayEquals(expected, bogoSort.sort(arr));
-  }
-
-  @Test
-  public void testCharacters() {
-    Character[] arr = new Character[] { 'g', 'f', 'e', 'd', 'c', 'b', 'a' };
-    Character[] expected = new Character[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g' };
-
-    Assert.assertArrayEquals(expected, bogoSort.sort(arr));
-  }
-
-  @Test
-  public void testDuplicateCharacters() {
-    Character[] arr = new Character[] { 'g', 'g', 'g', 'd', 'c', 'b', 'a' };
-    Character[] expected = new Character[] { 'a', 'b', 'c', 'd', 'g', 'g', 'g' };
-
-    Assert.assertArrayEquals(expected, bogoSort.sort(arr));
-  }
-
-  @Test
-  public void testStrings() {
-    String[] arr = new String[] { "aaaa", "aaaaa", "cca", "bba", "bbba", "zz" };
-    String[] expected = new String[] { "aaaa", "aaaaa", "bba", "bbba", "cca", "zz" };
-
-    Assert.assertArrayEquals(expected, bogoSort.sort(arr));
+  @Override
+  public Sorting createInstance() {
+    return new BogoSort();
   }
 }
---
+++ src/test/java/com/algorithms/sorting/SortingTest.java
@@ -0,0 +1,67 @@
+package com.algorithms.sorting;
+
+import com.algorithms.interfaces.Sorting;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public interface SortingTest<T extends Sorting> {
+
+  T createInstance();
+
+  @Test
+  public default void testPositiveIntegers() {
+    Integer[] arr = new Integer[] { 6, 2, 3, 0, 4, 5, 1, 7 };
+    Integer[] expected = new Integer[] { 0, 1, 2, 3, 4, 5, 6, 7 };
+
+    Assertions.assertArrayEquals(expected, this.createInstance().sort(arr));
+  }
+
+  @Test
+  public default void testNegativeIntegers() {
+    Integer[] arr = new Integer[] { -6, -2, -3, 0, -4, -5, -1, -7 };
+    Integer[] expected = new Integer[] { -7, -6, -5, -4, -3, -2, -1, 0 };
+
+    Assertions.assertArrayEquals(expected, this.createInstance().sort(arr));
+  }
+
+  @Test
+  public default void testMixedIntegers() {
+    Integer[] arr = new Integer[] { -50, 3, -1, 90, 77, 2, 5, 0 };
+    Integer[] expected = new Integer[] { -50, -1, 0, 2, 3, 5, 77, 90 };
+
+    Assertions.assertArrayEquals(expected, this.createInstance().sort(arr));
+  }
+
+  @Test
+  public default void testDuplicateIntegers() {
+    Integer[] arr = new Integer[] { 0, 1, 0, 1, 0, 0, 1, 1 };
+    Integer[] expected = new Integer[] { 0, 0, 0, 0, 1, 1, 1, 1 };
+
+    Assertions.assertArrayEquals(expected, this.createInstance().sort(arr));
+  }
+
+  @Test
+  public default void testCharacters() {
+    Character[] arr = new Character[] { 'g', 'f', 'e', 'd', 'c', 'b', 'a', 'w' };
+    Character[] expected = new Character[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'w' };
+
+    Assertions.assertArrayEquals(expected, this.createInstance().sort(arr));
+  }
+
+  @Test
+  public default void testDuplicateCharacters() {
+    Character[] arr = new Character[] { 'g', 'g', 'g', 'd', 'c', 'b', 'a', 'a' };
+    Character[] expected = new Character[] { 'a', 'a', 'b', 'c', 'd', 'g', 'g', 'g' };
+
+    Assertions.assertArrayEquals(expected, this.createInstance().sort(arr));
+  }
+
+  @Test
+  public default void testStrings() {
+    String[] arr = new String[] { "aaaa", "aaaaa", "cca", "bba", "bbba", "zz", "bba", "cca" };
+    String[] expected = new String[] { "aaaa", "aaaaa", "bba", "bba", "bbba", "cca", "cca", "zz" };
+
+    Assertions.assertArrayEquals(expected, this.createInstance().sort(arr));
+  }
+}
